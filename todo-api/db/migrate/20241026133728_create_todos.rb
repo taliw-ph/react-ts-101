7a96c223119f6b506d5439d2c11b74047a904190
Phanuwat Phoowichai <taliw@odds.team>
2024-10-26 20:45:23 +0700

    feat: 🎸 [API] Add default completed to todo

--- todo-api/db/migrate/20241026133728_create_todos.rb
+++ todo-api/db/migrate/20241026133728_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[7.2]
   def change
     create_table :todos do |t|
       t.string :text
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
